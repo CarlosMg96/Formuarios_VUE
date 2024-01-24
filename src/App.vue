3e1f38af2bd7eac7c3360199fcc86b3c3d3098a3
carlosmg96 <rguezcarlosmg96@outlook.com>
2024-01-24 13:12:38 -0600

    actividad finalizada de los formularios

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import Formulario from "./components/Xtra/Formulario.vue"
+</script>
+
+<template>
+<Formulario/>
+</template>
+
+<style scoped>
+
+</style>
